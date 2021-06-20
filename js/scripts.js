733971394bc9bd6033b88d0aa61452127c58f9e6
Zarifa Rasullu <zerife.resullu@gmail.com>
2021-06-20 23:57:37 +0400

    Refactor the code

--- js/scripts.js
+++ js/scripts.js
@@ -2,7 +2,11 @@
 let lowerPart = document.querySelector("#lower-part");
 let buttons = document.querySelectorAll("button");
 let part = document.querySelector(".part");
-let count = 0;
+let audio = document.querySelector("audio");
+
+let operand = "",
+ result,
+ array = [];
 //functions
 
 function add(a, b) {
@@ -40,45 +44,65 @@ function operate(op, a, b) {
 function Invert(num) {
  return -1 * Number(num);
 }
-let operand = "",
- array = [];
-function display(event) {
- if (!event.target) {
-  event.target = event;
- }
- let audio = document.querySelector("audio");
+function startAudio() {
  audio.currentTime = 0;
  audio.play();
+}
+function calculate(array) {
+ if (array[1] == "/") {
+  if (array[2] == "") {
+   array[2] = "1";
+  }
+  if (Number(array[0]) % Number(array[2]) == 0) {
+   result = parseInt(operate(array[1], Number(array[0]), Number(array[2])));
+  } else {
+   result = parseFloat(
+    operate(array[1], Number(array[0]), Number(array[2]))
+   ).toFixed(2);
+  }
+ } else {
+  if (array[1] == "*" && array[2] == "") {
+   array[2] = "1";
+  }
+  result = operate(array[1], Number(array[0]), Number(array[2]));
+ }
+ if (result.toString().includes(".")) {
+  result = parseFloat(result).toFixed(2);
+  console.log(result);
+ }
+ if (result.toString().length > 12) {
+  document.querySelector(".part").style["font-size"] = "120%";
+ }
+}
+function display(event) {
+ if (!event.target) {
+  //depending on keyevents. It may be keydown or click
+  event.target = event;
+ }
+ startAudio();
  if (/[0-9\.]+$/.test(event.target.innerText)) {
-  if (array.length == 1) {
+  if (array.length === 1) {
    return;
-  } else if (array.length == 0 || array.length == 2) {
+  } else if (array.length === 0 || array.length === 2) {
    operand += event.target.innerText;
    part.innerText = operand.substr(0, 8);
+   //I don't let to enter more than 8 digits
   }
-  [...document.querySelectorAll("button")].forEach((button) => {
-   button.style.border = "1px solid #2f3130";
-  });
  } else if (/[*\-+/]+/.test(event.target.innerText)) {
   if (event.target.innerText == "+/-") {
    part.innerText = -1 * part.innerText;
    operand = part.innerText;
   } else {
-   [...document.querySelectorAll("button")].filter((button) => {
-    if (button.innerText == event.target.innerText) {
+   [...document.querySelectorAll("button")].forEach((button) => {
+    if (button.innerText === event.target.innerText) {
      button.style.border = "1px solid #dbba35";
     } else {
      button.style.border = "1px solid #2f3130";
     }
    });
-   if (array.length == 0 || array.length == 2) {
-    if (event.target.innerText != "/" && event.target.innerText != "*") {
-     array.push(operand);
-     operand = "";
-    } else {
-     array.push(operand);
-     operand = "";
-    }
+   if (array.length === 0 || array.length === 2) {
+    array.push(operand);
+    operand = "";
    }
 
    if (array.length == 1) {
@@ -86,33 +110,7 @@ function display(event) {
    } else if (array.length == 2) {
     array[array.length - 1] = event.target.innerText;
    } else if (array.length === 3) {
-    let result;
-    if (array[1] == "/") {
-     if (array[2] == "") {
-      array[2] = "1";
-     }
-     if (array[1] == "/") {
-      if (Number(array[0]) % Number(array[2]) == 0) {
-       result = parseInt(operate(array[1], Number(array[0]), Number(array[2])));
-      } else {
-       result = parseFloat(
-        operate(array[1], Number(array[0]), Number(array[2]))
-       ).toFixed(2);
-      }
-     }
-    } else {
-     if (array[1] == "*" && array[2] == "") {
-      array[2] = "1";
-     }
-     result = operate(array[1], Number(array[0]), Number(array[2]));
-    }
-    if (result.toString().includes(".")) {
-     result = parseFloat(result).toFixed(2);
-     console.log(result);
-    }
-    if (result.toString().length > 12) {
-     document.querySelector(".part").style["font-size"] = "120%";
-    }
+    calculate(array);
 
     array = [];
     array.push(result);
@@ -122,46 +120,16 @@ function display(event) {
   }
  } else if (event.target.innerText === "=") {
   if (array.length == 0 || array.length == 2) {
-   if (event.target.innerText != "/" && event.target.innerText != "*") {
-    array.push(operand);
-    operand = "";
-   } else {
-    array.push(operand);
-    operand = "";
-   }
+   array.push(operand);
+   operand = "";
   }
-  if (array.length == 0 || array.length == 1 || array.length == 2) {
-   return;
-  } else {
-   let result;
-   if (array[1] == "/") {
-    if (array[2] == "") {
-     array[2] = "1";
-    }
-    if (Number(array[0]) % Number(array[2]) == 0) {
-     result = parseInt(operate(array[1], Number(array[0]), Number(array[2])));
-    } else {
-     result = parseFloat(
-      operate(array[1], Number(array[0]), Number(array[2]))
-     ).toFixed(2);
-    }
-   } else {
-    if (array[1] == "*" && array[2] == "") {
-     array[2] = "1";
-    }
-    result = operate(array[1], Number(array[0]), Number(array[2]));
-   }
-   if (result.toString().includes(".")) {
-    result = parseFloat(result).toFixed(2);
-    console.log(result);
-   }
-   if (result.toString().length > 12) {
-    document.querySelector(".part").style["font-size"] = "120%";
-   }
+  if (array.length == 3) {
+   calculate(array);
    array = [];
    array.push(result);
-   console.log(array);
    part.innerText = result;
+  } else {
+   return;
   }
  } else if (event.target.innerText == "AC") {
   console.log(part.innerText);
@@ -196,9 +164,7 @@ window.addEventListener("keydown", (e) => {
  [...buttons].forEach((button) => {
   if (e.which == button.getAttribute("data-key")) {
    display(button);
-   let audio = document.querySelector("audio");
-   audio.currentTime = 0;
-   audio.play();
+   startAudio();
   }
  });
 });
