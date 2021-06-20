6e625b737fc81a79bdae6a7c51c139b4262992cf
Zarifa Rasullu <zerife.resullu@gmail.com>
2021-06-20 21:45:06 +0400

    Add audio to calculator

--- js/scripts.js
+++ js/scripts.js
@@ -48,6 +48,7 @@ function display(event) {
  }
  let audio = document.querySelector("audio");
  audio.currentTime = 0;
+ audio.volume = 0.5;
  audio.play();
  if (/[0-9\.]+$/.test(event.target.innerText)) {
   if (array.length == 1) {
@@ -196,6 +197,7 @@ window.addEventListener("keydown", (e) => {
    display(button);
    let audio = document.querySelector("audio");
    audio.currentTime = 0;
+   audio.volume = 0.5;
    audio.play();
   }
  });
