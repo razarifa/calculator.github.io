8bf27816a140bdf4e62613fa7b2e9549fd486cd3
Zarifa Rasullu <zerife.resullu@gmail.com>
2021-06-20 15:59:02 +0400

    Works!

--- js/scripts.js
+++ js/scripts.js
@@ -44,12 +44,17 @@ let a = 0,
  b = 0,
  op,
  counter = 0;
-let array = [];
-
+let array = ["", "", ""];
+let p;
 function display(event) {
  if (/[0-9\.]+$/.test(event.target.innerText)) {
-  part.innerText += event.target.innerText;
-  array[2] = Number(part.innerText);
+  if (array[1] != "") {
+   part.innerText = "";
+   part.innerText = array[2] + event.target.innerText;
+   array[2] = part.innerText;
+  } else {
+   part.innerText += event.target.innerText;
+  }
  }
  if (event.target.innerText == "AC") {
   part.innerText = "";
@@ -60,7 +65,7 @@ function display(event) {
   part.innerText = num.join("");
  }
  if (event.target.innerText == "=") {
-  if (array[0] != undefined && array[1] != undefined && array[2] != undefined) {
+  if (array[0] != "" && array[1] != "" && array[2] != "") {
    if (array[1] == "/" && Number(array[0]) % Number(array[2]) == 0) {
     part.innerText = parseInt(
      operate(array[1], Number(array[0]), Number(array[2]))
@@ -69,31 +74,23 @@ function display(event) {
     part.innerText = operate(array[1], Number(array[0]), Number(array[2]));
    }
   }
-  array[1] = undefined;
-  array[0] = undefined;
-  array[2] = undefined;
+  array[1] = "";
+  array[0] = "";
+  array[2] = "";
  }
  if (/[*\-+/]+/.test(event.target.innerText)) {
-  if (event.target.innerText === "+/-") {
-   console.log(event.target.innerText);
-   part.innerText = Invert(part.innerText);
-  } else {
-   if (array[1] === undefined && array[0] === undefined) {
-    array[1] = event.target.innerText;
-    array[0] = part.innerText;
-    part.innerText = "";
+  if (array[1] != "") {
+   if (array[1] == "/" && Number(array[0]) % Number(array[2]) == 0) {
+    part.innerText = parseInt(
+     operate(array[1], Number(array[0]), Number(array[2]))
+    );
    } else {
-    if (array[1] == "/" && Number(array[0]) % Number(array[2]) == 0) {
-     part.innerText = parseInt(
-      operate(array[1], Number(array[0]), Number(array[2]))
-     );
-    } else {
-     part.innerText = operate(array[1], Number(array[0]), Number(array[2]));
-    }
-    array[1] = undefined;
-    array[0] = undefined;
-    array[2] = undefined;
+    part.innerText = operate(array[1], Number(array[0]), Number(array[2]));
    }
+   array[1] = event.target.innerText;
+  } else if (array[1] === "" && array[0] === "") {
+   array[1] = event.target.innerText;
+   array[0] = part.innerText;
   }
  }
 }
